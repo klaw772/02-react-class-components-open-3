f0489175ad8833579e1e79e3a432399c0c536b95
Kat Law <kathleen.law@multiverse.io>
2023-02-08 13:52:36 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const IMAGE_URL = "https://picsum.photos/350";
+const API_URL = "https://catfact.ninja/fact";
+function App () {
+
+    
+    return (
+         <h1>Hello World!</h1>
+    )
+
+}
+
+export { App };
